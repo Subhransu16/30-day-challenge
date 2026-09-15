49c7b4df4a4d5b10420e48e4c332642130fd041e
Subhransu16 <subhransuparida35@mail.com>
2026-09-15 23:33:47 +0530

    day6

---
+++ day6for.js
@@ -0,0 +1,15 @@
+//Create a for loop to find the sum of numbers from 1 to 100.
+let sum = 0;
+for (let i = 1; i<=100 ; i++){
+    sum += i;
+}
+console.log(sum);
+//Algorithm Explanation
+
+//First, we create a variable called `sum` and initialize it with `0`. 
+// Then we start a `for` loop with the variable `i` starting from `1`.
+//  The loop checks whether `i` is less than or equal to `100`. If the condition is true,
+//  the current value of `i` is added to `sum`. After each iteration, `i` is increased by
+//  `1`. This process continues until `i` reaches `100`. When the loop finishes,
+//  the value stored in `sum` is `5050`, which is the total of all numbers from `1` 
+// to `100`. Finally, `console.log(sum)` prints `5050` on the screen.
